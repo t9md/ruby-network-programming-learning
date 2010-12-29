843f3d9e37f4a31a820003408b4eb1dd97d2c961
t9md <taqumd@gmail.com>
2010-12-29 21:31:46 +0900

    modify

--- udp_sender.rb
+++ udp_sender.rb
@@ -1,6 +1,9 @@
 #!/usr/bin/env ruby
 require 'socket'
 sender = UDPSocket.open()
-sockaddr = Socket.pack_sockaddr_in(10000, "127.0.0.1")
+
+# sockaddr = Socket.pack_sockaddr_in(10000, "127.0.0.1")
+sockaddr = Socket.pack_sockaddr_in(10000, "255.255.255.255")
+sender.setsockopt(Socket::SOL_SOCKET, Socket::SO_BROADCAST, 1)
 sender.send("HELLO", 0, sockaddr)
 sender.close
